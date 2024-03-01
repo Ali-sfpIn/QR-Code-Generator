4d7328822590f39efb408e801d48693bcd50cb2c
Ali Koolivand <alisfp43@gmail.com>
2024-03-01 22:31:47 +0330

    initial commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+"use strict";
+// SELECTIONS...
+const inp = document.querySelector("input");
+const generateBtn = document.querySelector(".generate-btn");
+const qrCodeContainer = document.querySelector(".qr-code-container");
+// FUNCTIONS...
+
+// Fetching data using async await
+const fetcher = async function (query) {
+  const deliver = await fetch(
+    `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${query}`
+  );
+  qrCodeContainer.querySelector("img").src = deliver.url;
+};
+
+// EVENT HANDLERS...
+generateBtn.addEventListener("click", function () {
+  // Using Guard Clause
+  if (!inp.value) return;
+  fetcher(inp.value);
+});
