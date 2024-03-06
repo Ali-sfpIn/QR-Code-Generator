1138aaf61f282d54d524fba398c3688de540ef6c
Ali Koolivand <alisfp43@gmail.com>
2024-03-06 19:36:04 +0330

    not very important changes

--- js/script.js
+++ js/script.js
@@ -3,15 +3,15 @@
 const inp = document.querySelector("input");
 const generateBtn = document.querySelector(".generate-btn");
 const qrCodeContainer = document.querySelector(".qr-code-container");
-// FUNCTIONS...
 
+// FUNCTIONS...
 // Fetching data using async await
 const fetcher = async function (query) {
   try {
-    const deliver = await fetch(
+    const response = await fetch(
       `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${query}`
     );
-    qrCodeContainer.querySelector("img").src = deliver.url;
+    qrCodeContainer.querySelector("img").src = response.url;
   } catch (error) {
     console.error(`Error:${error.message}`);
   }
@@ -21,5 +21,7 @@ const fetcher = async function (query) {
 generateBtn.addEventListener("click", function () {
   // Using Guard Clause
   if (!inp.value) return;
+  // sending the query
   fetcher(inp.value);
 });
+// @js-challenges
